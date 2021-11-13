67f3bf4f3f3395e5140137894aeba21958abb1cd
Débora <deboraalexandra97@hotmail.com>
2021-11-13 16:24:26 +0000

    Added variables, makeGrid() and EventListener

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
 // Select color input
-// Select size input
+var color = document.getElementById("colorPicker").value;
+var container = document.getElementById("pixelCanvas");
+var size_choice = document.getElementById("sizePicker");
+var height = document.getElementById("inputHeight").value;
+var width = document.getElementById("inputWidth").value;
+
 
 // When size is submitted by the user, call makeGrid()
+size_choice.addEventListener("submit", function (event) {
+    event.preventDefault();
+    makeGrid(height, width);
+});
 
-function makeGrid() {
 
-// Your code goes here!
-
-}
+function makeGrid(height, width) { 
+    container.innerHTML = "";
+    for (var i = 0; i < height; i++); {
+        var row = document.createElement("tr");
+        for (var x = 0; x < width; x++); {
+            var cell = document.createElement("td");
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+};
