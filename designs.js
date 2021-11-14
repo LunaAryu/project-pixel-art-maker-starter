a15be7bfd3294530e8dc7c4b58d263dbaa732598
Débora <deboraalexandra97@hotmail.com>
2021-11-14 01:57:31 +0000

    Fixed some typos Added event listener to color cells

--- designs.js
+++ designs.js
@@ -1,10 +1,9 @@
-// Select color input
-var color = document.getElementById("colorPicker").value;
+// Select color input & define other variables
+var color = document.getElementById("colorPicker")
 var container = document.getElementById("pixelCanvas");
 var size_choice = document.getElementById("sizePicker");
-var height = document.getElementById("inputHeight").value;
-var width = document.getElementById("inputWidth").value;
-
+var height = document.getElementById("inputHeight");
+var width = document.getElementById("inputWidth");
 
 // When size is submitted by the user, call makeGrid()
 size_choice.addEventListener("submit", function (event) {
@@ -12,15 +11,24 @@ size_choice.addEventListener("submit", function (event) {
     makeGrid(height, width);
 });
 
-
+// This function generates a grid with given height/weight values
 function makeGrid(height, width) { 
     container.innerHTML = "";
-    for (var i = 0; i < height; i++); {
+    for (var i = 0; i < height.value; i++) {
         var row = document.createElement("tr");
-        for (var x = 0; x < width; x++); {
+        for (var x = 0; x < width.value; x++) {
             var cell = document.createElement("td");
             row.appendChild(cell);
         }
         container.appendChild(row);
     }
 };
+
+// Changes cell's color according to the user's choice
+container.addEventListener("click", function (event) {
+    console.log(event);
+    event.preventDefault();
+    if (event.target && event.target.nodeName == "TD") {
+        event.target.style.backgroundColor = color.value;
+    }   
+});
